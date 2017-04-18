3c4d4dceca0d3ede5159917e7f06196811e5cb09
Rob Di Marco <rob.dimarco@416software.com>
2017-04-17 23:29:08 -0400

    Add example ReactSelect with CSS

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,22 +4,82 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const FLAVOURS = [
+  { label: 'Chocolate', value: 'chocolate' },
+  { label: 'Vanilla', value: 'vanilla' },
+  { label: 'Strawberry', value: 'strawberry' },
+  { label: 'Caramel', value: 'caramel' },
+  { label: 'Cookies and Cream', value: 'cookiescream' },
+  { label: 'Peppermint', value: 'peppermint' },
+];
 
-Hello.defaultProps = {
-  name: 'David'
+const WHY_WOULD_YOU = [
+  { label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
+].concat(FLAVOURS.slice(1));
+
+class MultiSelectField extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      disabled: false,
+      crazy: false,
+      options: FLAVOURS,
+      value: [],
+    };
+    this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.toggleDisabled = this.toggleDisabled.bind(this)
+    this.toggleChocolate = this.toggleChocolate.bind(this)
+  }
+
+  handleSelectChange (value) {
+    console.log('You\'ve selected:', value);
+    this.setState({ value });
+  }
+
+  toggleDisabled (e) {
+    this.setState({ disabled: e.target.checked });
+  }
+
+  toggleChocolate (e) {
+    let crazy = e.target.checked;
+    this.setState({
+      crazy: crazy,
+      options: crazy ? WHY_WOULD_YOU : FLAVOURS,
+    });
+  }
+
+  render () {
+    return (
+      <div className="section">
+        <h3 className="section-heading">{this.props.label}</h3>
+        <Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your favourite(s)" options={this.state.options} onChange={this.handleSelectChange} />
+
+        <div className="checkbox-list">
+          <label className="checkbox">
+            <input type="checkbox" className="checkbox-control" checked={this.state.disabled} onChange={this.toggleDisabled} />
+            <span className="checkbox-label">Disable the control</span>
+          </label>
+          <label className="checkbox">
+            <input type="checkbox" className="checkbox-control" checked={this.state.crazy} onChange={this.toggleChocolate} />
+            <span className="checkbox-label">I do not like Chocolate (disabled the option)</span>
+          </label>
+        </div>
+      </div>
+    );
+  }
 }
 
-Hello.propTypes = {
-  name: React.PropTypes.string
+MultiSelectField.propTypes = {
+  label: React.PropTypes.string,
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <MultiSelectField />,
     document.body.appendChild(document.createElement('div')),
   )
 })
